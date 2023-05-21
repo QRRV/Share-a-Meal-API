000f2a1819f40b462514c4cde7f55d21b4799d93
QRRV <quinnie@hotmail.nl>
2023-05-21 22:55:19 +0200

    [FIX] again fixing the test

--- test/integration/mealControllerTest.js
+++ test/integration/mealControllerTest.js
@@ -91,7 +91,7 @@ describe('Manage meal', () => {
                 isVega: true,
                 isVegan: false,
                 isToTakeHome: true,
-                dateTime: Date.now().toString(),
+                dateTime: new Date().toISOString().slice(0, 19).replace('T', ' '),
                 imageUrl: "https://cdn.vox-cdn.com/thumbor/Si2spWe-6jYnWh8roDPVRV7izC4=/0x0:1192x795/1400x788/filters:focal(596x398:597x399)/cdn.vox-cdn.com/uploads/chorus_asset/file/22312759/rickroll_4k.jpg",
                 maxAmountOfParticipants: 2,
                 price: 10,
